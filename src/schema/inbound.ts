fafb58c0e47e29b579102d4fd4bca9335c414e4e
chidi-godwin <chidieberen1998@gmail.com>
2022-03-07 17:59:48 +0100

    add schema verification

---
+++ src/schema/inbound.ts
@@ -0,0 +1,12 @@
+import Joi from "joi";
+
+
+export const inboundValidator = Joi.object({
+    from: Joi.string().min(6).max(16).required(),
+    to: Joi.string().min(6).max(16).required(),
+    text: Joi.string().min(1).max(120).required(),
+    place: Joi.string().min(9)
+});
+
+
+
