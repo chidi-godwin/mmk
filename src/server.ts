faaeb4b7e533acc89e4adb9c980ae81f5fd248cf
chidi-godwin <chidieberen1998@gmail.com>
2022-03-07 12:42:10 +0100

    create basic server

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+import cors from "cors";
+import dotenv from "dotenv"
+dotenv.config({path: "./.env"});
+import { env } from "process";
+import express, { Request, Response } from "express";
+import { sequelize } from "./sequelize";
+
+
+const app = express();
+const port: Number = parseInt(env.PORT) || 8080;
+
+
+app.use(express.json());
+app.use(cors({
+    methods: 'POST',
+    preflightContinue: true,
+    origin: '*',
+}));
+
+app.use(async (req: Request, res: Response) => {
+    res.status(405).send({})
+});
+
+app.listen(port, async () => {
+    console.log(`Server is running on port ${port}`);
+    await sequelize.sync({ alter: true });
+    console.log("Database connected");
+    console.log(`press CTRL+C to stop server`);
+})
