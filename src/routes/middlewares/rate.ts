ee08a64d064027571ac99806251af02fd6454702
chidi-godwin <chidieberen1998@gmail.com>
2022-03-08 19:03:42 +0100

    remove custom redis store from rate limiter

--- src/routes/middlewares/rate.ts
+++ src/routes/middlewares/rate.ts
@@ -1,7 +1,6 @@
 import dotenv from "dotenv"
 dotenv.config();
 import rateLimit from "express-rate-limit";
-import RedisStore from "rate-limit-redis";
 import RedisClient from "ioredis";
 import { env } from "process";
 
@@ -13,9 +12,5 @@ export const limiter = rateLimit({
   max: 50, 
   standardHeaders: true,
   legacyHeaders: false,
-
-  store: new RedisStore({
-    // @ts-expect-error - Known issue: the `call` function is not present in @types/ioredis
-    sendCommand: (...args: string[]) => client.call(args),
-  }),
+  message: "too many requests, limit reached"
 });
