3beb98786fbfbe9ce447b97e65a5d1f02146d4cb
chidi-godwin <chidieberen1998@gmail.com>
2022-03-08 02:17:32 +0100

    heroku config files

--- src/config/config.ts
+++ src/config/config.ts
@@ -4,21 +4,6 @@ import { Dialect } from "sequelize/types";
 import { env } from "process";
 
 
-const {
-    DB_USERNAME,
-    DB_PASSWORD,
-    DB_DATABASE,
-    DB_HOST,
-    DB_PORT,
-} = env;
+const { DB_URI } = env;
 
-const dialect: Dialect = 'postgres';
-
-export const config = {
-    username: DB_USERNAME,
-    password: DB_PASSWORD,
-    database: DB_DATABASE,
-    host: DB_HOST,
-    port: parseInt(DB_PORT),
-    dialect
-};
+export const config = { DB_URI }
