0fac11a5e33a97096bc54b12c4e7ddb06d45a641
chidi-godwin <chidieberen1998@gmail.com>
2022-03-08 04:26:05 +0100

    revert back tp TLS

--- src/services/outbound.ts
+++ src/services/outbound.ts
@@ -9,7 +9,7 @@ import { env } from "process";
 
 export async function outboundService(data: Sms, user: Account): Promise<ServiceResponse> {
     const client = createClient({
-        url: env.REDIS_TLS_URL
+        url: env.REDIS_URL
     });
 
     client.on('error', (err) => console.log('Redis Client Error', err));
